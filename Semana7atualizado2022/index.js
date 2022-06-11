da100dab8ddc4c559d2012663825c63075804667
YvesGabriel <102773265+YvesGabriel@users.noreply.github.com>
2022-06-11 00:06:21 -0300

    Add files via upload

---
+++ Semana7atualizado2022/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+
+
+app.listen(45678,() => {
+    console.log("Pai ta on");
+});
